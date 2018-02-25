fa5d2c51d83fdd5694992a456dddc4e567f46e57
MoreThanInfinity <sansay30.07.1992@gmail.com>
2018-02-25 18:19:21 +0300

    some changes whit registration

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :users do
     member do
