3f7a79c69fd0e92ba7883751637ca3c7cca9701b
MoreThanInfinity <sansay30.07.1992@gmail.com>
2018-02-27 02:06:25 +0300

    last step

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://obscure-reef-22257.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://obscure-reef-22257.herokuapp.com']
+  config.action_cable.url = 'wss://my-social-network.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://my-social-network.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
